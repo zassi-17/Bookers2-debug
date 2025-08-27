578c583388701a03f25dfb42f41940fffb1ae24d
Ubuntu <ubuntu@ip-172-31-25-78.ap-northeast-1.compute.internal>
2025-08-27 23:21:45 +0000

    [finish]いいね機能実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  has_many :favorites,dependent: :destroy
+
+  def favorited_by?(user)
+     favorites.exists?(user_id: user.id)
+  end
 end
