5261e5da3eb617742943df0ec529b32a1441ff2d
Ubuntu <ubuntu@ip-172-31-25-78.ap-northeast-1.compute.internal>
2025-08-31 13:09:22 +0000

    [finish]コメント機能、非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,13 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-  end
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    @book_comment = BookComment.new
+    end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.destroy
+    @book_comment = BookComment.new
   end
 
   def book_comment_params
